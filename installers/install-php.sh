c7eb4f64ef89219da69e13e65e46f7bb71b27274
Magnus Eriksson <mange@reloop.se>
2018-01-31 23:01:16 +0100

    Removed missing package from php7.2

--- installers/install-php.sh
+++ installers/install-php.sh
@@ -11,7 +11,7 @@ sudo apt install php7.1 php7.1-common php7.1-cli php7.1-curl php7.1-fpm php7.1-g
 echo "***********************************************************"
 echo "* Installing PHP 7.2                                      *"
 echo "***********************************************************"
-sudo apt install php7.2 php7.2-common php7.2-cli php7.2-curl php7.2-fpm php7.2-gd php7.2-intl php7.2-json php7.2-mbstring php7.2-mcrypt php7.2-mysql php7.2-pgsql php7.2-sqlite3 php7.2-tidy php7.2-xml php7.2-zip -y
+sudo apt install php7.2 php7.2-common php7.2-cli php7.2-curl php7.2-fpm php7.2-gd php7.2-intl php7.2-json php7.2-mbstring php7.2-mysql php7.2-pgsql php7.2-sqlite3 php7.2-tidy php7.2-xml php7.2-zip -y
 
 echo "***********************************************************"
 echo "* Installing PHP 7 Shared                                 *"
